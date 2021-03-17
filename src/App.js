2afa766607512c4ccbb5115778d90187f52fa204
Michele Cheow <ldnallen@gmail.com>
2021-03-17 13:52:58 -0700

    add game rest button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import "./App.scss";
-import React, { Component } from "react";
+import React from "react";
 import Header from "./components/Header/Header";
 import GameBoard from "./components/GameBoard/GameBoard";
-import { Component } from "react";
+import villagers from "./data/data.js";
 
-export default class App extends Component {
-  constructor() {
-    super();
-  }
+const App = () => {
+  const allVillagers = villagers.concat(villagers);
 
-  showModal = () => {
-    this.setState({ isShown: true }, () => {
-      this.closeButton.focus();
+  const newGame = () => {
+    const allMatched = document.querySelectorAll(".game__card--match");
+    const totalScore = 0;
+    const score = document.querySelector(".score");
+    score.textContent = `${totalScore}`;
+
+    allMatched.forEach((card) => {
+      card.classList.remove("game__card--match");
     });
+
+    allVillagers.sort(() => 0.5 - Math.random());
   };
 
-  render() {
-    return (
-      <main className="game game__wrap" role="main">
-        <div className="container">
-          <Header newGame={this.newGame} />
-          <GameBoard />
-        </div>
-      </main>
-    );
-  }
-}
+  return (
+    <main className="game game__wrap" role="main">
+      <div className="container">
+        <Header newGame={newGame} />
+        <GameBoard />
+      </div>
+    </main>
+  );
+};
+
+export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,30 +3,32 @@ import Button from "../Button/Button";
 import logo from "../../assets/logo.png";
 import bells from "../../assets/bell.png";
 
-export default function Header() {
-  return (
-    <header className="game__header">
-      <div className="game__logo">
-        <img src={logo} alt="Animal Crossing New Horizons Logo" />
-      </div>
-      <div className="game__score-wrap">
-        <div className="game__btn">
-          <Button
-            variant="primary"
-            size="lg"
-            aria-label="New Game"
-            aria-labelledby="new-game"
-            handleChange={this.props.newGame}
-          >
-            New Game
-          </Button>
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className="game__header">
+        <div className="game__logo">
+          <img src={logo} alt="Animal Crossing New Horizons Logo" />
         </div>
-        <div className="game__score">
-          <img src={bells} alt="Animal Crossing Bell Bag" />
-          <span className="score">0</span>
-          <span className="game__score--text">Bells</span>
+        <div className="game__score-wrap">
+          <div className="game__btn">
+            <Button
+              variant="primary"
+              size="lg"
+              aria-label="New Game"
+              aria-labelledby="new-game"
+              handleChange={this.props.newGame}
+            >
+              New Game
+            </Button>
+          </div>
+          <div className="game__score">
+            <img src={bells} alt="Animal Crossing Bell Bag" />
+            <span className="score">0</span>
+            <span className="game__score--text">Bells</span>
+          </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  }
 }
