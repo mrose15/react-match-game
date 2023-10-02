8e54dff62e582e3aa51cecc5ed3ba9114ca721c2
Michele Cheow <ldnallen@gmail.com>
2023-10-02 06:57:20 -0700

    started to refactor react match game app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import "./App.scss";
 import React from "react";
 import Header from "./components/Header/Header";
 import GameBoard from "./components/GameBoard/GameBoard";
-import villagers from "./data/data.js";
+
+import "./App.scss";
 
 const App = () => {
-  const allVillagers = villagers.concat(villagers);
-
-  const newGame = () => {
-    const allMatched = document.querySelectorAll(".game__card--match");
-    const totalScore = 0;
-    const score = document.querySelector(".score");
-    score.textContent = `${totalScore}`;
-
-    allMatched.forEach((card) => {
-      card.classList.remove("game__card--match");
-    });
-
-    allVillagers.sort(() => 0.5 - Math.random());
-  };
-
   return (
-    <main className="game game__wrap" role="main">
+    <main className="game game__wrap">
       <div className="container">
-        <Header newGame={newGame} />
+        <Header />
         <GameBoard />
       </div>
     </main>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,32 +3,32 @@ import Button from "../Button/Button";
 import logo from "../../assets/logo.png";
 import bells from "../../assets/bell.png";
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <header className="game__header">
-        <div className="game__logo">
-          <img src={logo} alt="Animal Crossing New Horizons Logo" />
+const Header = () => {
+  return (
+    <header className="game__header">
+      <div className="game__logo">
+        <img src={logo} alt="Animal Crossing New Horizons Logo" />
+      </div>
+      <div className="game__score-wrap">
+        <div className="game__btn">
+          <Button
+            variant="primary"
+            size="lg"
+            aria-label="New Game"
+            aria-labelledby="new-game"
+            handleChange={this.props.newGame}
+          >
+            New Game
+          </Button>
         </div>
-        <div className="game__score-wrap">
-          <div className="game__btn">
-            <Button
-              variant="primary"
-              size="lg"
-              aria-label="New Game"
-              aria-labelledby="new-game"
-              handleChange={this.props.newGame}
-            >
-              New Game
-            </Button>
-          </div>
-          <div className="game__score">
-            <img src={bells} alt="Animal Crossing Bell Bag" />
-            <span className="score">0</span>
-            <span className="game__score--text">Bells</span>
-          </div>
+        <div className="game__score">
+          <img src={bells} alt="Animal Crossing Bell Bag" />
+          <span className="score">0</span>
+          <span className="game__score--text">Bells</span>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
